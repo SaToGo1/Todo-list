6d729c767f017ee9fc47582882aac82ff690ac4f
SaToGo <stoledano97@gmail.com>
2022-09-09 21:36:04 +0200

    Add the initial layout

--- dist/main.js
+++
@@ -1,1 +0,0 @@
-(()=>{const e=document.createElement("h3");e.textContent="Webpack setup for to-do list",document.querySelector("body").append(e)})();
--- src/index.js
+++
@@ -1,7 +0,0 @@
-const title = document.createElement('h3');
-
-
-title.textContent = 'Webpack setup for to-do list';
-
-const page = document.querySelector('body');
-page.append(title);
