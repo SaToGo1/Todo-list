0418797a9eeb9807d6bb70c9d2d6f98ac1b779d5
SaToGo <stoledano97@gmail.com>
2022-11-17 22:02:56 +0100

    ongoing add Project Button

--- src/DOM/addProject.js
+++ src/DOM/addProject.js
@@ -2,16 +2,39 @@ export default class addProject {
     constructor(){
         this.addProjectButton = document.getElementById("addProject");
         this.addProjectContainer = document.getElementById("addProjectContainer");
-        this.addProjectAccept = document.getElementById("sidebar__accept");
-        this.addProjectCancel = document.getElementById("sidebar__cancel");
+        //this.addProjectAccept = document.getElementById("sidebar__accept");
+        //this.addProjectCancel = document.getElementById("sidebar__cancel");
+        this.addProjectSidebarContainer = document.getElementById("sidebar__projects");
     }
 
     addProjectClick = (action) => {
         this.addProjectButton.addEventListener('click', ()=>{
-            this.addProjectContainer.innerHTML = HTMLTemplateAddProjectName();
+            let div = document.createElement('div');
+            div.className = "sidebar__container-addingProject sidebar__addProject";
+            div.id = "sidebar__addProject"
+            div.innerHTML = HTMLTemplateAddProjectName();
+
+            this.addProjectContainer.style.display = 'none';
+            this.addProjectSidebarContainer.append(div);
+
+            div.button.addEventListener('click', ()=>{
+                this.addAcceptCancelClick();
+            })
         })
     }
-  
+
+    addAcceptCancelClick = () => {
+        this.addProjectButton.addEventListener('click', ()=>{
+            let addProject = document.getElementById("sidebar__addProject");
+            let projectName = document.getElementById("sidebar__ProjectNameInput").value;
+
+            let div = document.createElement('div');
+            div.className = "sidebar__container";
+            div.innerHTML = HTMLTemplateCreateNewProject(projectName);
+
+            this.addProjectSidebarContainer.append(div);
+        })
+    }
 }
 
 
@@ -22,7 +45,18 @@ export default class addProject {
 function HTMLTemplateAddProjectName(){
     
     return `
-    <div>
-        <input type=text>
-    </div>`
+        <label for="sidebar__ProjectNameInput">Project Name:</label>
+        <input type="text" id="sidebar__ProjectNameInput">
+        <button class="sidebar__button sidebar__accept" id="sidebar__accept">Accept</button>
+        <button class="sidebar__button sidebar__cancel" id="sidebar__cancel">Cancel</button>
+    `
+}
+
+function HTMLTemplateCreateNewProject(projectName){
+    
+    return `
+    <img src="./img/icon.svg" alt="icon" class="sidebar__icon">
+    <button class="sidebar__button">${projectName}</button>
+    <button class="sidebar__delete">X</button>
+    `
 }
