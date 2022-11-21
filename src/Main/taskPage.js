d2e22ad823eb61e266f4dbfd8bf66ccdbc3806ac
SaToGo <stoledano97@gmail.com>
2022-11-21 18:32:48 +0100

    Add CreateNewTaskElements function
    in taskPage.js

--- src/Main/taskPage.js
+++ src/Main/taskPage.js
@@ -1,25 +1,36 @@
 export default class taskPage {
     constructor(){
         this.addTaskDiv = document.getElementById("addTaskDiv");
+        //this.addTaskButton = this.addTaskDiv.childNodes[0];
+        //this.addTaskName = this.addTaskDiv.childNodes[1];
         
-        /* let tasks = []*/ 
     }
 
     addTaskEvent = () => {
-        
+
     }
 
 }
 
-/*
-function CreateNewTaskElements(){
+function CreateNewTaskElements(name){
  
     let div = document.createElement('div');
+    div.className = "content__task";
 
- return `<div class="content__task">
-            <span class="contentTask__icon"></span>
-            <p class="contentTask__text">This is a second task</p>
-            <button class="contentTask__delete">X</button>
-        </div>`
+    let icon = document.createElement('span');
+    icon.className = "contentTask__icon";
+
+    let p = document.createElement('p');
+    p.className = "contentTask__text";
+    p.textContent = name;
+
+    let button = document.createElement('button');
+    p.className = "contentTask__delete";
+    p.textContent = "X";
+
+    div.append(icon);
+    div.append(p);
+    div.append(button);
+
+    return div;
 }
-*/
