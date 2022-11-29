cd0320960e0aaf0fdb5ab1922995fb75b65d4903
SaToGo <stoledano97@gmail.com>
2022-11-29 14:11:03 +0100

    Add change to active style when clicking on a page in the sidebar

--- src/index.js
+++ src/index.js
@@ -16,14 +16,26 @@ let month = new taskPage("Month");
 home.loadPage();
 
 //Select the main 4 butons on sidebar.
-//(The ones that are not in projects).
+//(The ones that are not in projects, home, today, ...).
 let sidebartimes = document.getElementById('sidebar__times');
 let sidebarButtonArray = sidebartimes.getElementsByClassName('sidebar__container');
 
-//Put event listener on everyone of the main 4 buttons.
+//Put event listener on everyone of the main 4 buttons(home, ...).
 for(let i = 0, len = sidebarButtonArray.length; i < len; i++){
-    let projectButton = sidebarButtonArray[i]
-    projectButton.addEventListener('click', () => {
-        eval(projectButton.textContent.toLowerCase()).loadPage();
+    let pageButton = sidebarButtonArray[i]
+    pageButton.addEventListener('click', () => {
+        eval(pageButton.textContent.toLowerCase()).loadPage();
+    })
+}
+
+//Event to change the active sidebar button.
+let sidebarContainerArray = document.getElementsByClassName('sidebar__container');
+for(let i = 0, len = sidebarContainerArray.length; i < len; i++){
+    let sidebarContainer = sidebarContainerArray[i];
+    sidebarContainer.addEventListener('click', () => {
+        for(let j = 0, len = sidebarContainerArray.length; j < len; j++){
+            sidebarContainerArray[j].classList.remove('sidebar__container-active');
+        }
+        sidebarContainer.classList.add('sidebar__container-active');
     })
 }
