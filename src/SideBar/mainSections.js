fb670da6a998abf28d25a2ee1eca6476e66d0e86
SaToGo <stoledano97@gmail.com>
2022-11-30 23:45:59 +0100

    add mainSections.js and projectData.js
    projectData will save the data of the projects we make.
    
    mainSection.js will save the code of the interaction with the
    main 4 buttons on sidebar, (home, today, week, month).
     - initialize() method.
         #set up the event listener for the buttons.
    
    moved code from index.js to mainSections.js in constructor and
    initialize().

---
+++ src/SideBar/mainSections.js
@@ -0,0 +1,38 @@
+import taskPage from "../Main/taskPage";
+
+export default class  {
+    constructor(){        
+        this.home = new taskPage("Home");
+        this.today = new taskPage("Today");
+        this.week = new taskPage("Week");
+        this.month = new taskPage("Month");
+    }
+
+    initialize = () => {
+        this.home.loadPage();
+
+        let sidebarTimes = document.getElementById('sidebar__times');
+        let sidebarButtonArray = sidebarTimes.getElementsByClassName('sidebar__container');
+        //Put event listener on everyone of the main 4 buttons(home, ...).
+        for(let i = 0, len = sidebarButtonArray.length; i < len; i++){
+            let pageButton = sidebarButtonArray[i]
+            pageButton.addEventListener('click', () => {
+                //example of eval()-> this.home.loadPage();
+                eval( "this." + pageButton.textContent.toLowerCase()).loadPage();
+            })
+        }
+
+        //Care to see if it actualizes with new projects.
+        //Event to change the active sidebar button.
+        let sidebarContainerArray = document.getElementsByClassName('sidebar__container');
+        for(let i = 0, len = sidebarContainerArray.length; i < len; i++){
+            let sidebarContainer = sidebarContainerArray[i];
+            sidebarContainer.addEventListener('click', () => {
+                for(let j = 0, len = sidebarContainerArray.length; j < len; j++){
+                    sidebarContainerArray[j].classList.remove('sidebar__container-active');
+                }
+                sidebarContainer.classList.add('sidebar__container-active');
+            })
+        }
+    }
+}
---
+++ src/SideBar/projectData.js
@@ -0,0 +1,19 @@
+export default class projectData {
+    constructor(){        
+        this.projectArray = [];
+    }
+
+    /*
+    saveProject = (title) => {
+        let proj = new project(title);
+        this.projectArray.push(proj);
+    }*/
+
+
+}
+
+/*class project {
+    constructor(title){
+        this.title = title;
+    }
+}*/
