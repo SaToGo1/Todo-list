052f1d78f83af9dac6893e6f0a466ce35d26ca6e
SaToGo1 <stoledano97@gmail.com>
2023-07-14 16:43:21 +0200

    Trying to make github secrrets work '._.

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -8,12 +8,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY || null,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN || null,
-  projectId: process.env.FIREBASE_PROJECT_ID || null,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET || null,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || null,
-  appId: process.env.FIREBASE_APP_ID || null,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 let app = null;
@@ -38,4 +38,3 @@ try {
 } catch (error) {
   console.log('error in the firebase connect \n', error)
 }
-// testing github secrets
